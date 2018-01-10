6aaccc138d71ed90a1841748ba2ec8b9ed79c038
Sujai Sivasamy <sujai.mca2012@gmail.com>
2018-01-10 21:33:41 +0530

    Removed unwanted files

--- data.sql
+++
@@ -1,6 +0,0 @@
-insert into quiz(question, expected, answer) values 
-								('mysql-q1', 'a1', 'a'), 
-								('mysql-q2', 'a2', 'b'),
-								('mysql-q3', 'a3', 'c'),
-								('mysql-q4', 'a4', 'd'),
-								('mysql-q5', 'a5', 'e');
